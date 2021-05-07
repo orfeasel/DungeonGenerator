21278b5f59f1b933cbe2b3562aff360fd25cf489
Orfeas Eleftheriou <orfeas.eleftheriou@gmail.com>
2021-05-07 12:14:24 +0300

    Added Dungeon Gen Dev Project

---
+++ Source/DungeonGeneratorDev/DungeonGeneratorDevGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "DungeonGeneratorDevGameModeBase.h"
+
---
+++ Source/DungeonGeneratorDev/DungeonGeneratorDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DungeonGeneratorDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNGEONGENERATORDEV_API ADungeonGeneratorDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
