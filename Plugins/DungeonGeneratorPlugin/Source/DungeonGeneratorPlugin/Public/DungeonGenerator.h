3b6d769ea4d780b8fbd54af46044d68af2ee8944
Orfeas Eleftheriou <orfeas.eleftheriou@gmail.com>
2021-05-07 12:17:58 +0300

    Added Dungeon Generator Plugin to project

---
+++ Plugins/DungeonGeneratorPlugin/Source/DungeonGeneratorPlugin/Public/DungeonGenerator.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DungeonGenerator.generated.h"
+
+UCLASS()
+class DUNGEONGENERATORPLUGIN_API ADungeonGenerator : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADungeonGenerator();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
